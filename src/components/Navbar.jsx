831a29fa23ec924339d9520328ca4ff990086a03
thinhpv01 <thinhpv01@gmail.com>
2023-08-04 10:40:01 +0700

    ☎ Contact

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Thinhpv &nbsp;
-            <span className="sm:block hidden"> | JS Mastery</span>
+            <span className="sm:block hidden"> | Javascript</span>
           </p>
         </Link>
 
